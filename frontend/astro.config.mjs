b95527d41de06e6f71cd4dd36d2e13d51ffc3179
Felix <nmcuong23@clc.fitus.edu.vn>
2025-10-03 13:46:54 +1000

    added more pages, testing with tailwindcss

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
+
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
+import vue from '@astrojs/vue';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+      plugins: [tailwindcss()],
+      resolve: {
+        alias: {
+          '@components': '/src/components',
+          '@layouts': '/src/layouts',
+          '@pages': '/src/pages',
+          '@styles': '/src/styles',
+        },
+      },
+	},
+
+  integrations: [vue()],
+});
